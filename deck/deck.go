555934e45458108b56bb50ee2de786eddda8b460
devk <devkordesg@gmail.com>
2017-08-18 00:46:52 +0200

    Draw implemented

--- deck/deck.go
+++ deck/deck.go
@@ -1,7 +1,17 @@
 package deck
 
 import (
+    // "fmt"
+    "time"
+    "math/rand"
 )
+
+/**
+ * Simple deck
+ * Some rules:
+ * * Cards are assumed to be unique (one of each max)
+ * * Top card is card with index 0
+ */
 
 type Deck struct {
     cards []Card
@@ -9,4 +19,37 @@ type Deck struct {
 
 func NewDeck(cards []Card) Deck {
     return Deck{cards: cards}
+}
+
+func (d *Deck) Cards() []Card {
+    return d.cards
+}
+
+func (d *Deck) Card(i int) Card {
+    return d.cards[i]
+}
+
+func (d *Deck) Shuffle() {
+    cards := d.Cards()
+    clen := len(cards)
+    for i := clen - 1; i > 0; i-- {
+        rand.Seed(time.Now().Unix())
+        min := 0
+        max := i
+        swapi := rand.Intn(max - min) + min
+        cards[swapi], cards[i] = cards[i], cards[swapi]
+    }
+}
+
+func (d *Deck) Count() int {
+    return len(d.Cards())
+}
+
+func (d *Deck) Draw() Card {
+    if d.Count() == 0 {
+        return NewCard("")
+    }
+    c := d.Card(0)
+    d.cards = d.cards[1:]
+    return c
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -4,6 +4,39 @@ import (
     "testing"
 )
 
+func testCards() []Card {
+    return []Card{
+        Card{face: "H", value: "2"},
+        Card{face: "H", value: "3"} }
+}
+
+func testDeck() Deck {
+    return NewDeck(testCards())
+}
+
+func testEqCard(a, b []Card) bool {
+
+    if a == nil && b == nil {
+        return true;
+    }
+
+    if a == nil || b == nil {
+        return false;
+    }
+
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i := range a {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+
+    return true
+}
+
 func TestNewDeck(t *testing.T) {
     cards := []Card{
         Card{face: "H", value: "2"},
@@ -17,5 +50,40 @@ func TestNewDeck(t *testing.T) {
     deck = NewDeck([]Card{})
     if len(deck.cards) != 0 {
         t.Fatal("NewDeck(0 cards) len = ", len(deck.cards), " expected 0")
+    }
+}
+
+func TestCards(t *testing.T) {
+    deck := testDeck()
+    if !testEqCard(testCards(), deck.Cards()) {
+        t.Fatal("testDeck.Cards() does not equal input cards")
+    }
+}
+
+func TestCard(t *testing.T) {
+    deck := testDeck()
+    if deck.Card(0) != testCards()[0] {
+        t.Fatal("testDeck.Card(0) does not equal input card 0")
+    }
+    if deck.Card(1) != testCards()[1] {
+        t.Fatal("testDeck.Card(1) does not equal input card 1")
+    }
+}
+
+func TestDraw(t *testing.T) {
+    deck := testDeck()
+    c := deck.Draw()
+    if c != testCards()[0] {
+        t.Fatal("testDeck.Draw() does not equal input card 0")
+    }
+    if len(deck.cards) != 1 {
+        t.Fatal("testing.Draw() count is ", len(deck.cards), " should be 1")
+    }
+    c = deck.Draw()
+    if c != testCards()[1] {
+        t.Fatal("testDeck.Draw() 2 does not equal input card 1")
+    }
+    if len(deck.cards) != 0 {
+        t.Fatal("testing.Draw() 2 count is ", len(deck.cards), " should be 0")
     }
 }
