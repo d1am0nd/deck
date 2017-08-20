1826aada12b843c796bd17359498471c08003b38
devk <devkordesg@gmail.com>
2017-08-20 23:57:04 +0200

    someting

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
     "fmt"
+    "cards/deck"
 )
 
 func main() {
+    d := deck.NewDeck([]deck.Card{deck.NewCard("Th"),
+        deck.NewCard("5h"),
+        deck.NewCard("As"),
+        deck.NewCard("3h"),
+        deck.NewCard("7s"),
+        deck.NewCard("2c")})
+
+    fmt.Println(d.Cards())
+    d.Shuffle()
+    fmt.Println(d.Cards())
+
     fmt.Println("Hello world")
 }
